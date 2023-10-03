bac799829dc258b2f8a670ace460e872fd17f899
Sendi <49103067+SardorAlien@users.noreply.github.com>
2023-10-03 10:38:05 +0100

    Gson to Json initial experiment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
+import com.google.gson.Gson
+
 fun main(args: Array<String>) {
     println("Hello mom!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    //Gson to JSON
+    var colors = HashMap<Int, String>()
+    colors[1] = "Blue"
+    colors[2] = "Red"
+    colors[3] = "Purple"
+
+    var gson = Gson()
+
+    var output = gson.toJson(colors)
+
+    println(output);
 }
